51c9a4c07662126ed469391637ab09830799491a
Adam Oberle <oberle.adam@gmail.com>
2023-05-08 19:11:52 -0500

    buttons successfully remove hp/ap from totals

--- script.js
+++ script.js
@@ -16,21 +16,61 @@ function onReady() {
     // There are lots of buttons and things ready for you to hook into here!
     
     $('.arcane-scepter').on( 'click', scepterAttack )
+    $('.entangle').on( 'click', entangleAttack )
+    $('.dragon-blade').on( 'click', dragonBladeAttack )
+    $('.star-fire').on( 'click', starFireAttack )
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
 }
 
-function render(AP, HP){
+function render(){
 
     $('.ap-text').text(currentAttackPoints)
+    $('.hp-text').text(currentHP)
+
 }
 
 function scepterAttack(event){
     event.preventDefault();
     console.log( currentAttackPoints );
-    currentAttackPoints -= 14;
+    currentAttackPoints -= 12;
+    currentHP -= 14;
+
+    if( currentHP <= 0 ){
+        $('.enemy').addClass('freaky-fungus dead')
+    }
+
+    render();
+}
+
+function entangleAttack(event){
+    event.preventDefault();
+    console.log( currentAttackPoints );
+    currentAttackPoints -= 23;
+    currentHP -= 9;
+
+
+    render();
+}
+
+function dragonBladeAttack(event){
+    event.preventDefault();
+    console.log( currentAttackPoints );
+    currentAttackPoints -= 38;
+    currentHP -= 47;
+
+
+    render();
+}
+
+function starFireAttack(event){
+    event.preventDefault();
+    console.log( currentAttackPoints );
+    currentAttackPoints -= 33;
+    currentHP -= 25;
 
 
     render();

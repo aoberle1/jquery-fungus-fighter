5a19dbb8b7766bf071a4192591f047992c16568d
Adam Oberle <oberle.adam@gmail.com>
2023-05-08 19:32:06 -0500

    mushroom jumps if AP reduced to 0

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function onReady() {
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
+    // registering event handlers for button clicks
     $('.arcane-scepter').on( 'click', scepterAttack )
     $('.entangle').on( 'click', entangleAttack )
     $('.dragon-blade').on( 'click', dragonBladeAttack )
@@ -28,11 +29,20 @@ function onReady() {
 
 function render(){
 
+    // setting the value of these elements to these variables
     $('.ap-text').text(currentAttackPoints)
     $('.hp-text').text(currentHP)
 
+    // if statement setup
     if( currentHP <= 0 ){
+        // adds class freaky-fungus dead
         $('.enemy').addClass('freaky-fungus dead')
+        //removes class freaky-fungus walk completely (removes it from itself)
+        $('.freaky-fungus.walk').removeClass('freaky-fungus walk')
+    }
+
+    if (currentAttackPoints <= 0){
+        $('.enemy').addClass('freaky-fungus jump')
         $('.freaky-fungus.walk').removeClass('freaky-fungus walk')
     }
 }
@@ -42,9 +52,6 @@ function scepterAttack(event){
     console.log( currentAttackPoints );
     currentAttackPoints -= 12;
     currentHP -= 14;
-
-
-
     render();
 }
 
@@ -53,8 +60,6 @@ function entangleAttack(event){
     console.log( currentAttackPoints );
     currentAttackPoints -= 23;
     currentHP -= 9;
-
-
     render();
 }
 
@@ -63,8 +68,6 @@ function dragonBladeAttack(event){
     console.log( currentAttackPoints );
     currentAttackPoints -= 38;
     currentHP -= 47;
-
-
     render();
 }
 
@@ -73,7 +76,5 @@ function starFireAttack(event){
     console.log( currentAttackPoints );
     currentAttackPoints -= 33;
     currentHP -= 25;
-
-
     render();
 }

0c10289ccf36ae54b0d975df74bbb90e887f2998
Adam Oberle <oberle.adam@gmail.com>
2023-05-08 19:11:28 -0500

    initial commit

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let currentAttackPoints = 100;
+let currentHP = 100;
+
 function onReady() {
+    console.log( 'jq' );
+    render();
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
-    
+    $('.arcane-scepter').on( 'click', scepterAttack )
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function render(AP, HP){
+
+    $('.ap-text').text(currentAttackPoints)
+}
+
+function scepterAttack(event){
+    event.preventDefault();
+    console.log( currentAttackPoints );
+    currentAttackPoints -= 14;
+
+
+    render();
 }
